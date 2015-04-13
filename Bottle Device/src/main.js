5262fb4f3a9980f4c2a3b4e543ef556f5544b2cd
abafzal <abafzal1993@berkeley.edu>
2015-04-13 15:48:36 -0700

    Added desired_temp label, fixed 32/70 degree issue

--- Bottle Device/src/main.js
+++ Bottle Device/src/main.js
@@ -7,10 +7,12 @@ var labelStyle = new Style( { font: "bold 20px", color:"black" } );
 // Internal Variables
 phoneURL = "";
 temperature_unit = "\xB0 F"; //default temperature to Fahrenheit
-current_temperature = 70; // range of temp should be 32 degrees for freezing to 212 degrees for boiling
+current_temperature = 32; // range of temp should be 32 degrees for freezing to 212 degrees for boiling
 current_temperature_string = current_temperature + temperature_unit;
-old_slider = 70;
-new_slider = 70;
+desired_temperature = 32; 
+desired_temperature_string = desired_temperature + temperature_unit;
+old_slider = 32;
+new_slider = 32;
 
 // Handler calls
 Handler.bind("/discover", Behavior({
@@ -39,9 +41,13 @@ Handler.bind("/updateTemperature", Behavior({
 	    handler.invoke(new Message(phoneURL + "currentTemperature"), Message.TEXT);
 	},
 	onComplete: function(handler, message, text) {
-	    current_temperature = parseFloat(text);
-	    current_temperature_string = current_temperature + temperature_unit;
-	    current_temperature_label.string = current_temperature_string;
+		desired_temperature = parseFloat(text);
+		desired_temperature_string = desired_temperature + temperature_unit;
+		desired_temperature_label.string = desired_temperature_string;
+		trace("desired temp " + desired_temperature);
+	    //current_temperature = parseFloat(text);
+	    //current_temperature_string = current_temperature + temperature_unit;
+	    //current_temperature_label.string = current_temperature_string;
 	}
 }));
 
@@ -68,7 +74,7 @@ Handler.bind("/potResult", Object.create(Behavior.prototype, {
 				}
 			}}
 }));
-
+var desired_temperature_label = new Label({left:0, right:0, height:40, width:70, string:desired_temperature, style: labelStyle});
 var current_temperature_label = new Label({left:0, right:0, height:40, width:70, string:current_temperature, style: labelStyle});
 var last_time_updated = new Label({left:20, right:20, height:40, string:new Date(), style: labelStyle});
 var survival_mode_switch = new Label({left:0, right:0, height:40, width:80, string:"OFF", style: labelStyle});
@@ -77,6 +83,12 @@ var mainColumn = new Column({
 	left: 0, right: 0, top: 0, bottom: 0, skin: whiteSkin,
 	contents: [
 	    new Line({left:10, right:10, top:10, bottom:0,
+				contents:[
+				    new Label({left:20, right:0, width: 160, height:40, string:"Desired Temperature:", style: labelStyle}),
+				    desired_temperature_label,
+      			]
+			}),
+		new Line({left:10, right:10, top:10, bottom:0,
 				contents:[
 				    new Label({left:20, right:0, width: 160, height:40, string:"Current Temperature:", style: labelStyle}),
 				    current_temperature_label,
@@ -125,7 +137,7 @@ MainCanvas.behaviors[0] = Behavior.template({
 				application.invoke( new MessageWithObject( "pins:/potentiometers/read?repeat=on&callback=/potResult&interval=70" ) );
 	},
 	receiveSliderChange: function(params, data) {
-	    new_slider = (data.slider*180 + 32).toFixed(2);
+	    new_slider = (data.slider*180 + 32).toFixed(0);
 	},
 })
 var mainCanvas = new MainCanvas();
--- Bottle Phone/src/home.js
+++ Bottle Phone/src/home.js
@@ -4,8 +4,8 @@
 function updateTemperature(newTemp) {
     // make sure current_temperature is up to date before calling this to update device
     current_temperature = newTemp;
-    current_temperature_string = current_temperature + temperature_unit;
-    current_temperature_label.string = current_temperature_string;
+    //current_temperature_string = current_temperature + temperature_unit;
+    //current_temperature_label.string = current_temperature_string;
     updateDeviceTemperature(current_temperature);
 }
 
@@ -56,10 +56,10 @@ var cold_button = new SmallTextButtonTemplate({textForLabel:"c", temp: 40});
 var room_button = new SmallTextButtonTemplate({textForLabel:"r", temp: 72});
 var hot_button = new SmallTextButtonTemplate({textForLabel:"h", temp: 150});
 var boil_button = new SmallTextButtonTemplate({textForLabel:"b", temp: 212});
-var water_button = new SmallTextButtonTemplate({textForLabel:"water", temp: current_temperature});
-var tea_button = new SmallTextButtonTemplate({textForLabel:"tea", temp: current_temperature});
-var coffee_button = new SmallTextButtonTemplate({textForLabel:"coffee", temp: current_temperature});
-var iceTea_button = new SmallTextButtonTemplate({textForLabel:"iced tea", temp: current_temperature});
+//var water_button = new SmallTextButtonTemplate({textForLabel:"water", temp: current_temperature});
+//var tea_button = new SmallTextButtonTemplate({textForLabel:"tea", temp: current_temperature});
+//var coffee_button = new SmallTextButtonTemplate({textForLabel:"coffee", temp: current_temperature});
+//var iceTea_button = new SmallTextButtonTemplate({textForLabel:"iced tea", temp: current_temperature});
 
 // Labels
 // var current_temperature_label = new Label({string: current_temperature_string, style:textStyle, skin: blueSkin});
@@ -99,9 +99,9 @@ exports.homeCol = new Column({
 				new Line({left:0, right:0, top:0, bottom:0,
 					contents:[ cold_button, room_button, hot_button, boil_button]
 				}),
-				new Line({left:0, right:0, top:0, bottom:0,
-					contents:[ water_button, tea_button, coffee_button, iceTea_button]
-				}),
+				//new Line({left:0, right:0, top:0, bottom:0,
+					//contents:[ water_button, tea_button, coffee_button, iceTea_button]
+				//}),
 			]
 		}),
 	],
